bcc266ecb29d5370f961c39061f2fb5c067e1110
Jackparo <zingme321@gmail.com>
2020-05-29 16:58:37 +0700

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const port = 3000;
+
+let userRoute = require('./routes/user.route');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { name:'Nam'});
+});
+
+app.use('/users', userRoute);
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
